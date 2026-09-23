83282f1e70c6ed56151bfd1120e9a6653e62a71b
raoulmunet <raoulmunet@gmail.com>
2026-09-23 19:23:38 +0300

    Add install.sql

---
+++ install.sql
@@ -0,0 +1,10 @@
+prompt Installing Oracle Reconciliation Engine...
+
+@sql/01_tables.sql
+@sql/02_indexes.sql
+@sql/03_sample_data.sql
+@sql/04_reporting_views.sql
+@plsql/pkg_reconciliation.pks
+@plsql/pkg_reconciliation.pkb
+
+prompt Installation complete.
